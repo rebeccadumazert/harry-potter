e87951f91a91e32bd20f74c58a5499845f3cf03b
dumazert <rebeccadumazert@hotmail.fr>
2020-04-22 18:11:57 +0200

    globalstyle font-face

--- client/src/components/styles/header.js
+++ client/src/components/styles/header.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import './index.css';
 
 export const Head = styled.div`
   color: red;
@@ -10,5 +11,6 @@ export const StyledLink = styled(Link)`
   padding: 30px;
   color: black;
   text-decoration: none;
-  font-family: 'Harry Potter';
+  font-family: 'HarryPotter';
+  font-size: 40px;
 `;
